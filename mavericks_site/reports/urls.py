3402a5af0baac9ca0ee9bc2a07af337c4a576291
Jen Merritt <jenjomez@gmail.com>
2020-04-24 15:51:32 +0100

    update report delete report routes

--- mavericks_site/reports/urls.py
+++ mavericks_site/reports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ReportListView, ReportDetailView, ReportCreateView
+from .views import ReportListView, ReportDetailView, ReportCreateView, ReportUpdateView, ReportDeleteView
 
 urlpatterns = [
     path("", ReportListView.as_view(), name='reports-home'),
     path('report/<int:pk>/', ReportDetailView.as_view(), name='report-detail'),
     path('report/new/', ReportCreateView.as_view(), name='report-create'),
+    path('report/<int:pk>/update', ReportUpdateView.as_view(), name='report-update'),
+    path('report/<int:pk>/delete', ReportDeleteView.as_view(), name='report-delete'),
 ]
--- mavericks_site/reports/views.py
+++ mavericks_site/reports/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Report
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class ReportListView(ListView):
     model = Report
@@ -22,3 +22,28 @@ class ReportCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class ReportUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Report
+    fields = ['title', 'content', 'date']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        report = self.get_object()
+        if self.request.user == report.author:
+            return True
+        return False
+
+
+class ReportDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Report
+    success_url = '/'
+
+    def test_func(self):
+        report = self.get_object()
+        if self.request.user == report.author:
+            return True
+        return False
