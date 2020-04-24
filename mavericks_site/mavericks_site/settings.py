c68de4cf84706fdcf0ebfa65a9ff621bbaf5bb3a
Jen Merritt <jenjomez@gmail.com>
2020-04-24 15:18:34 +0100

    login and out

--- mavericks_site/mavericks_site/settings.py
+++ mavericks_site/mavericks_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'reports',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'reports-home'
--- mavericks_site/reports/urls.py
+++ mavericks_site/reports/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import ReportListView, ReportDetailView
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", ReportListView.as_view(), name='reports-home'),
     path('report/<int:pk>/', ReportDetailView.as_view(), name='report-detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
