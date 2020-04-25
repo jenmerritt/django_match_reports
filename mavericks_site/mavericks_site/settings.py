3130daaadf0d0a8f2aaf8b56e2518d0189ff264d
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-04-25 13:11:21 +0100

    update settings

--- mavericks_site/mavericks_site/settings.py
+++ mavericks_site/mavericks_site/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u*6jn%oye*jt$ni+m)@@$%*ht!n1ac)a9pq3xj4pm^*200_my('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = []
 
